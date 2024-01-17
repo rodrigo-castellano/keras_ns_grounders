f2c541a9909bf90cabc6f1c516e05fdbbadbe00f
rodrigocastellano <rodrigo.castellano@unisi.it>
2024-01-18 00:41:57 +0100

    small changes

--- experiments/countries/runner_rules_hparam.py
+++ experiments/countries/runner_rules_hparam.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     epochs: int = 10
     assert epochs > 0
 
-    DATASET_NAME = ['kinship_family','nations_AMIE','nations_NCRL','countries_s1','countries_s2','countries_s3']
+    DATASET_NAME = ['nations_AMIE','nations_NCRL','countries_s1','countries_s2','countries_s3']
     SEED = [[0,1,2,3,4]]
     E = [100] 
     DROPOUT = [0.0]
