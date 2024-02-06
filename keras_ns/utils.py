ad769c30e4228b35bc7c8b63326be01068df8958
rodrigo-castellano <castellano97@gmail.com>
2024-02-06 16:07:14 +0100

    slight changes

--- keras_ns/utils.py
+++ keras_ns/utils.py
@@ -359,8 +359,8 @@ class FileLogger():
 
                 string_args = string_args.replace(' ', '')
                 string_args = string_args.replace('\n', '')
-                print('\nlast_line\n', last_line)
-                print('string_args\n', string_args)
+                # print('\nlast_line\n', last_line)
+                # print('string_args\n', string_args)
                 # look for all the keys in the last line 
                 # for k in keys:
                 #     if k not in last_line:
@@ -371,7 +371,7 @@ class FileLogger():
                 #         string_args = "_".join(string_args_split[:pos] + string_args_split[pos+2:])
                 # print('2string_args\n', string_args)
                 if string_args in last_line:
-                    print('string_args in last_line')
+                    # print('string_args in last_line')
                     return True 
 
                 # file_values = [str(a) for a in filename.split("_")]
