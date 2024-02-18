3f6d7146cee26bde121b9c4ddbae2340721be2b5
rodrigo-castellano <rodrigo.castellano@unisi.it>
2024-02-18 20:36:47 +0100

    Now it works

--- experiments/countries/test_gpu.py
+++ experiments/countries/test_gpu.py
@@ -1,2 +1,18 @@
 import tensorflow as tf
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+
+import torch
+
+# Check if CUDA (GPU support) is available
+if torch.cuda.is_available():
+    # Get the number of available GPUs
+    num_gpus = torch.cuda.device_count()
+    print(f"Number of GPUs available: {num_gpus}")
+    # Get the name of each GPU
+    for i in range(num_gpus):
+        print(f"GPU {i}: {torch.cuda.get_device_name(i)}")
+else:
+    print("CUDA is not available. No GPUs detected.")
+
+print(torch.backends.cudnn.version())
