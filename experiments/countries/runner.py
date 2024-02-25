b19fbbc7b7ad3d3f7b1e8b1fe1a64c1b51cf6ed2
rodrigo-castellano <rodrigo.castellano@unisi.it>
2024-02-25 23:21:46 +0100

    slight changes

--- experiments/countries/runner.py
+++ experiments/countries/runner.py
@@ -23,7 +23,7 @@ import argparse
 if __name__ == '__main__':
 
 
-    base_path :str = "data"
+    base_path :str = "./data"
     epochs: int = 100
     assert epochs > 0
     DATASET_NAME = ['FB15K']#['countries_s1','countries_s2','countries_s3','kinship_family','pharmkg_small','nations',] #['countries_s1','countries_s2','countries_s3','pharmkg_small','pharmkg_small_reason_2','pharmkg_full','nations','kinship_family_small','kinship_family','kinship_family_reason_2' ] 
