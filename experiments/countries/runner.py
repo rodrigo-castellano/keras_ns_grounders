012aa0cc9fe678af8a92b3e251122f28df548df6
rodrigo-castellano <rodrigo.castellano@unisi.it>
2024-02-25 23:22:41 +0100

    slight changes

--- experiments/countries/runner.py
+++ experiments/countries/runner.py
@@ -79,6 +79,8 @@ if __name__ == '__main__':
         if not os.path.exists(os.path.join(base_path, dataset_name)):
             print('skipping, dataset not existing', run_vars)
             print(os.path.join(base_path, dataset_name))
+            # show the files in the directory
+            print(os.listdir(os.path.join(base_path, dataset_name)))
             continue
 
         if 'countries' in dataset_name:
