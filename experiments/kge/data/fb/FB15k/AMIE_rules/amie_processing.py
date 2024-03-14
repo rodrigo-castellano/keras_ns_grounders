1a9be5ca7fe693c1a587044c0a43713e648381dd
rodrigo-castellano <63149239+rodrigo-castellano@users.noreply.github.com>
2024-03-14 09:18:33 +0100

    results for the michelangelos changes in kge and dataset.py

--- experiments/kge/data/fb/FB15k/AMIE_rules/amie_processing.py
+++
@@ -1,8 +0,0 @@
-with open('../freebase_mtr100_mte100-train.txt') as f:
-    lines = f.readlines()
-    lines = [line.replace('\t', ' ') for line in lines]
-file = open('train_amie_processed.txt', 'w+')
-for line in lines:
-    file.write(line)
-
-print('end')
