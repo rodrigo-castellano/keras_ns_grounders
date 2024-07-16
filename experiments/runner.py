df0b9b4cd3bf12d2af0f20de6e69522fea57e941
rodrigo-castellano <rodrigo.castellano@unisi.it>
2024-07-16 18:05:18 +0200

    update patience in early stopping

--- experiments/runner.py
+++ experiments/runner.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     ckpt_folder :str = None #os.path.join(log_folder,'checkpoints')
     checkpoint_load = False
     base_path :str = "experiments/data"
-    epochs: int = 250
+    epochs: int = 200
     EARLY_STOPPING = True
     GLOBAL_SERIALIZATION = False
     LLM = False
