678c2b114b7a4fe4e2ce79f1a326fc2bfe9592ba
rodrigo-castellano <rodrigo.castellano@unisi.it>
2024-07-18 09:25:01 +0200

    slight changes

--- experiments/runner.py
+++ experiments/runner.py
@@ -41,8 +41,8 @@ if __name__ == '__main__':
     ckpt_folder :str = None #os.path.join(log_folder,'checkpoints')
     checkpoint_load = False
     base_path :str = "experiments/data"
-    epochs: int = 100
-    EARLY_STOPPING = False
+    epochs: int = 150
+    EARLY_STOPPING = True
     GLOBAL_SERIALIZATION = False
     LLM = False
     ULTRA = False
@@ -61,7 +61,7 @@ if __name__ == '__main__':
     R = [0.0]
     RR = [0.0]
     LR = [0.01] #[0.01]
-    LR_SCHEDULER = ['None'] #['plateau'] # None
+    LR_SCHEDULER = ['plateau'] #['plateau'] # None
     OPTIMIZER = ['adam'] #['adam'] #['None','adam']
     NUM_RULES = [1] 
     VALID_SIZE = [None]
