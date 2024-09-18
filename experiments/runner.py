40b17b40e60ba4d0116b70c6552ebedd0c0c8fbb
rodrigo-castellano <rodrigo.castellano@unisi.it>
2024-09-18 10:17:21 +0200

    included saving weights

--- experiments/runner.py
+++ experiments/runner.py
@@ -124,10 +124,10 @@ if __name__ == '__main__':
         args.batch_size = 256 # Full batch only for explain.
         args.val_batch_size = 256
         # args.test_batch_size = 128 if dataset_name in heavy_datasets_domainbody_relationentity else 256
-        if dataset_name in 'wn18rr' or dataset_name == 'FB15k237':
+        if dataset_name in 'wn18rr' or dataset_name == 'FB15k237' or dataset_name == 'pharmkg_full' :
+            args.test_batch_size = 1
+        elif dataset_name == 'kinship_family':
             args.test_batch_size = 4
-        elif dataset_name == 'pharmkg_full' or dataset_name == 'kinship_family':
-            args.test_batch_size = 16
         elif dataset_name in 'countries_s3':
             args.test_batch_size = 128
         else:
