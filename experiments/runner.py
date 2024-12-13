08e072a9eae49b5967a3a4e67be9c9a0a8cac604
rodrigo-castellano <rodrigo.castellano@unisi.it>
2024-12-13 18:56:56 +0100

    starting exp with pharmkg

--- experiments/runner.py
+++ experiments/runner.py
@@ -42,15 +42,15 @@ if __name__ == '__main__':
     data_path :str = "experiments/data"
     epochs: int = 100
     EARLY_STOPPING = True
-    DATASET_NAME = ['countries_s1','countries_s2','countries_s3'] #['ablation_d2'] #['countries_s2','countries_s3','nations','kinship_family','pharmkg_small','pharmkg_full','wn18rr','nations','FB15k237']
-    GROUNDER = ['backward_1_3'] # ['backward_0_1','backward_0_2','backward_0_3','backward_1_1','backward_1_2','backward_1_3',
+    DATASET_NAME = ['pharmkg_small'] #['ablation_d2'] #['countries_s2','countries_s3','nations','kinship_family','pharmkg_small','pharmkg_full','wn18rr','nations','FB15k237']
+    GROUNDER = ['backward_1_1','backward_1_2','backward_1_3','backward_0_1','backward_0_2','backward_0_3'] # ['backward_0_1','backward_0_2','backward_0_3','backward_1_1','backward_1_2','backward_1_3',
                # 'backward_2_1','backward_2_2','backward_2_3',] # 'domainbody','relationentity','full']
-    KGE = ['transe'] # ["distmult", "transe","complex", "rotate"]
-    MODEL_NAME = ['r2n'] # ['no_reasoner','dcr','sbr','r2n'] 
+    KGE = ['complex'] # ["distmult", "transe","complex", "rotate"]
+    MODEL_NAME = ['no_reasoner','dcr','sbr','r2n'] 
     RULE_MINER = ['amie','None'] 
     E = [100]
     DEPTH = [1]
-    SEED = [[0]]# [[0,1,2,3,4]]
+    SEED = [[0,1,2,3,4]]
     NEG_PER_SIDE = [1]
     WEIGHT_LOSS = [.5]  
     DROPOUT = [0.0]
