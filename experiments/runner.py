9626936940087fedf1f073652d429e22ea7bbc20
rodrigo-castellano <63149239+rodrigo-castellano@users.noreply.github.com>
2025-01-29 12:18:59 +0100

    Update runner.py

--- experiments/runner.py
+++ experiments/runner.py
@@ -31,7 +31,7 @@ wandb.login()
 if __name__ == '__main__':
 
     use_logger = True
-    use_WB = False
+    use_WB = True
     load_model_ckpt = False
     load_kge_ckpt = False
     save_model_ckpt = True
